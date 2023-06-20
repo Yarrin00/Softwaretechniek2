1f6f4e40d470b3669378970511624a900f2e0461
Yarrin00 <80321705+Yarrin00@users.noreply.github.com>
2023-06-20 18:03:31 +0200

    Create Main.cpp

---
+++ Week1/Opdracht_5.13.1/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int inproduct(std::vector<int> u, std::vector<int> v) {
+  int result = 0;
+  for (size_t i = 0; i < u.size(); i++) {
+    result += u[i] * v[i];
+  }
+  return result;
+}
+
+int main() {
+  std::vector<int> x = {1, 2, 3, 4};
+  std::vector<int> y = {4, 3, 2, 1};
+  std::cout << "inproduct = " << inproduct(x, y) << std::endl;
+  return 0;
+}
